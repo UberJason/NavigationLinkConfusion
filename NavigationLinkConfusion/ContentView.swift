ebdd53c902a46be55e9688ee3fa2a62552ee5e3c
Jason Ji <uberjason@gmail.com>
2021-10-20 16:39:36 -0400

    Project showing a behavioral difference in NavigationLink between iOS 14 and 15.

--- NavigationLinkConfusion/ContentView.swift
+++ NavigationLinkConfusion/ContentView.swift
@@ -5,17 +5,78 @@
 //  Created by Jason Ji on 10/20/21.
 //
 
+import Combine
 import SwiftUI
 
+struct Plan: Identifiable {
+    let id = UUID()
+    let title: String
+    let detail: Detail
+}
+
+struct Detail: Identifiable {
+    let id = UUID()
+    let title: String
+}
+
+class Model: ObservableObject {
+    @Published var plans = [Plan]()
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        refreshPlans()
+        
+        NotificationCenter.default.publisher(for: .refresh)
+            .sink { [unowned self] _ in
+                self.refreshPlans()
+            }
+            .store(in: &cancellables)
+    }
+    
+    func refreshPlans() {
+        plans = [
+            Plan(title: "Plan 1", detail: Detail(title: "Detail 1"))
+        ]
+    }
+}
+
 struct ContentView: View {
+    @StateObject var model = Model()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(model.plans) { plan in
+                NavigationLink(destination:
+                    DetailView(title: plan.detail.title)
+                , label: {
+                    Text(plan.title)
+                })
+            }
+            .listStyle(InsetGroupedListStyle())
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Notification.Name {
+    static let refresh = Notification.Name(rawValue: "refresh")
+}
+
+struct DetailView: View {
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text(title)
+            Button("Refresh") {
+                NotificationCenter.default.post(name: .refresh, object: nil)
+            }
+        }
     }
 }
